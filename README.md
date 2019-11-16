8b2253e38a5e7831ab58bdbb74695e9866f88023
manu <emmanuel.boudreault@polymtl.ca>
2019-11-16 12:37:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Polynômes
+
+This is a simple react app to practice polynomial division.
+
+To start a local instance:
+
+```
+yarn install
+yarn start
+```
---
+++ src/App.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import './App.css';
+import _ from 'lodash';
+
+interface Elem {
+    scalar: number,
+    degree: number,
+}
+
+type Poly = Elem[];
+
+const make_poly = (): Poly => simplify_poly(
+    [
+        { scalar: _.random(-5, 5), degree: 3 },
+        { scalar: _.random(-5, 5), degree: 2 },
+        { scalar: _.random(-5, 5), degree: 1 },
+        { scalar: _.random(-5, 5), degree: 0 },
+    ],
+);
+
+const cartesian = (a: Poly, b: Poly): Poly[] => _.concat([], ...a.map((d: Elem) => b.map((e: Elem) => _.concat(d, e))));
+
+const simplify_poly = (poly: Poly): Poly => _(poly)
+    .groupBy((elem) => elem.degree)
+    .values()
+    .map((elems) => {
+        const scalar = _(elems)
+            .map(x => x.scalar)
+            .sum();
+        const first = _.first(elems);
+        return {
+            scalar,
+            degree: (first && first.degree) || 0,
+        };
+    })
+    .filter((x: Elem) => x.scalar != 0)
+    .value();
+
+const multiply_poly = (a: Poly, b: Poly): Poly => simplify_poly(
+    _(cartesian(a, b))
+        .map((x: Elem[]): Elem => {
+            const a: Elem = x[0];
+            const b: Elem = x[1];
+            return {
+                scalar: a.scalar * b.scalar,
+                degree: a.degree + b.degree,
+            };
+        })
+        .value()
+);
+
+const poly_to_string = (poly: Poly): string => {
+    const expts = ['', 'x', 'x²', 'x³', 'x⁴', 'x⁵', 'x⁶'];
+
+    return _(poly)
+        .sort((a, b) => b.degree - a.degree)
+        .reduce((acc, { scalar, degree }) => {
+            if (!acc) {
+                return `${scalar}${expts[degree]}`;
+            }
+
+            const sign = scalar >= 0 ? '+' : '-';
+            return `${acc} ${sign} ${Math.abs(scalar)}${expts[degree]}`
+        }, '');
+};
+
+const App = () => {
+    const [state, setState] = useState({
+        a: make_poly(),
+        b: make_poly(),
+        visible: false
+    });
+    const c = multiply_poly(state.a, state.b);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <a onClick={ () => setState({
+                a: make_poly(),
+                b: make_poly(),
+                visible: false,
+            })}>
+              new problem
+            </a>
+          </p>
+          <p>
+            { poly_to_string(c) }
+          </p>
+          <p>
+            { poly_to_string(state.a) }
+          </p>
+          <p>
+            { state.visible && poly_to_string(state.b) }
+            { !state.visible &&
+              <a onClick={ () => setState({
+                  ...state,
+                  visible: !state.visible,
+              }) }>
+                answer
+              </a>
+            }
+          </p>
+        </header>
+      </div>
+    );
+};
+
+export default App;
